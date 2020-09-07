2da6688161bb259c85ca53631f521112b598bbba
Jitse0 <69904272+Jitse0@users.noreply.github.com>
2020-09-07 09:32:23 +0200

    Add files via upload

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,6 +1,6 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
@@ -9,4 +9,12 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+
+  noStroke();
+  fill('pink');
+  triangle(0,225,450,0,450,450);
+
+  noStroke();
+  fill('purple');
+  triangle(0,0,0,450,450,225);
 }
